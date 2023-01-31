da282806920a36fdbf77fc1843f3255e0b2efbeb
raissa <kouabenanraissa@yahoo.fr>
2023-01-31 08:13:08 +0000

    first commit

---
+++ exercises.php
@@ -0,0 +1,34 @@
+
+<?php
+//Exercise1
+    echo 'Hello World';
+?>
+
+<?php
+
+//Exercise2
+$title = 'HEllo Php';
+
+echo $title;
+?>
+
+<?php
+//Exercise3
+$salut = 'Bonjour';
+$journée = 'Bonne journée!';
+echo $salut , ".".$journée;
+
+?>
+
+
+<?php
+//Exercise4
+        $title = 'Salut a tous et bonne nuit ';
+        function Inverse($val){
+        return strrev($val);
+  };
+
+echo Inverse($title);     
+    ?>
+
+
